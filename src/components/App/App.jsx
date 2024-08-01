e254587a8029522ff9061d2acdbe4262d550a345
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-01 17:41:18 +0200

    Update_2

---
+++ src/components/App/App.jsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import css from "./App.module.css";
+import Navigation from "../Navigation/Navigation";
+import { Suspense, lazy } from "react";
+import ScrollToTopButton from "../ScrollToTopButton/ScrollToTopButton";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("../../pages/MovieDetailsPage/MovieDetailsPage")
+);
+const NotFoundPage = lazy(() => import("../../pages/NotFoundPage"));
+const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
+const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
+
+function App() {
+  return (
+    <div className={css.box}>
+      <Navigation />
+      <ScrollToTopButton />
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+export default App;
