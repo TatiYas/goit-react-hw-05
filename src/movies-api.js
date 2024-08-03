9a107cfda3b5f3db530572d22817e3cc00c1648b
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-03 17:38:31 +0200

    Update_8

--- src/movies-api.js
+++ src/movies-api.js
@@ -1,44 +1,59 @@
 import axios from 'axios';
-import { API_PATH } from 'constants/api';
-
-
-
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-axios.defaults.headers = {
-	Authorization: 'https://api.themoviedb.org/3/movie/550?api_key=1eca7b963c30880cf3d9bb1257b1e20b',
-	accept: 'application/json',
-};
-// axios.defaults.params = {
-// 	language: 'en-US',
-// };
 
-export const fetchTrendMovies = async () => {
-	const response = await axios.get(API_PATH.trend, {});
-	return response.data;
+const options = {
+    headers: {
+        Authorization:
+            'https://api.themoviedb.org/3/movie/550?api_key=1eca7b963c30880cf3d9bb1257b1e20b',
+    },
 };
 
-export const fetchSearchMovie = async (query, page = 1) => {
-	const response = await axios.get(API_PATH.search, {
-		params: {
-			query,
-			page,
-		},
-	});
+export const getTrendingMovies = async () => {
+    const url = '/trending/movie/day';
 
-	return response.data;
+    const response = await axios.get(url, options);
+    return response.data.results;
+};
+export const getMovieDetails = async (movieId) => {
+    const url = `/movie/${movieId}`;
+
+    const response = await axios.get(url, options);
+    return response.data;
+};
+export const searchMovies = async (searchQuery) => {
+    const url = `/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1`;
+
+    const response = await axios.get(url, options);
+    return response.data;
 };
 
-export const fetchMovieById = async (id) => {
-	const response = await axios.get(API_PATH.movie + id + '?');
-	return response.data;
+export const getMovieCredits = async (movieId) => {
+    const url = `/movie/${movieId}/credits`;
+
+    const response = await axios.get(url, options);
+    return response.data;
 };
 
-export const fetchMovieCredits = async (id) => {
-	const response = await axios.get(API_PATH.movie + id + '/credits?');
-	return response.data;
+export const getMovieReviews = async (movieId) => {
+    const url = `/movie/${movieId}/reviews`;
+
+    const response = await axios.get(url, options);
+    return response.data;
 };
 
-export const fetchMovieReview = async (id) => {
-	const response = await axios.get(API_PATH.movie + id + '/reviews?');
-	return response.data;
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
