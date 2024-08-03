d2d56e2ee96e24ae157dab08ecfac5acd07eabd4
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-03 18:22:19 +0200

    Update_14

---
+++ src/components/Pages/MoviesPage/MoviesPage.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { searchMovies } from "../../movies-api";
+import MovieList from "../../components/MovieList/MovieList";
+import Loader from "../../components/Loader/Loader";
+import SearchForm from "../../components/SearchForm/SearchForm";
+import NotFoundPage from "../NotFoundPage";
+import s from "./MoviesPage.module.css";
+
+function MoviesPage() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query") ?? "";
+
+  useEffect(() => {
+    if (query) {
+      const getMovies = async () => {
+        try {
+          setIsLoading(true);
+          const data = await searchMovies(query);
+          setMovies(data.results);
+        } catch (error) {
+          setError(true);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+      getMovies();
+    }
+  }, [query]);
+
+  return (
+    <div className={s.box}>
+      <SearchForm />
+      {isLoading && <Loader />}
+      {error && <NotFoundPage />}
+      <MovieList movies={movies} />
+    </div>
+  );
+}
+
+export default MoviesPage;
