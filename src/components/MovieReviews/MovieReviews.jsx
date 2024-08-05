7ca740ce38acf6d07881d09bd30a3dc34e56528d
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-05 19:22:00 +0200

    Update_28

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,13 +1,30 @@
-
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchReviewsById } from "../../movies-api";
 
 const MovieReviews = () => {
-  return <div></div>;
+  const params = useParams();
+
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetchReviewsById(params.movieId).then((data) => setReviews(data.results));
+  }, [params.movieId]);
+
+  return (
+    <div>
+      <ul>
+        {reviews.map((review) => (
+          <li key={review.id}>
+            {review.author}
+            <p>
+              {review.content ? review.content : "No reviews for this movie."}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MovieReviews;
-
-
-
-
-
