b40b6db652fb414c92f1d79afc48a0deab132b72
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-05 18:09:17 +0200

    Update_26

--- src/components/Pages/MoviesPage/MoviesPage.jsx
+++ src/components/Pages/MoviesPage/MoviesPage.jsx
@@ -1,88 +1,35 @@
-import { Field, Form, Formik } from "formik";
-import toast, { Toaster } from "react-hot-toast";
-import * as Yup from "yup";
+import SearchBar from "../../SearchBar/SearchBar";
+import MovieList from "../../MovieList/MovieList";
 import { useEffect, useState } from "react";
-import MoviesList from "../../MovieList/MovieList";
+import { fetchSearchMovies } from "../../../movies-api";
 import { useSearchParams } from "react-router-dom";
-import { fetchMoviesSearch } from "../../../movies-api";
 
 const MoviesPage = () => {
-  const [moviesSearch, setMoviesSearch] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const [isError, setIsError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
 
-  const searchValue = searchParams.get("query") ?? "";
+  const filterValue = searchParams.get("query") ?? "";
 
-  const initialValues = {
-    search: "",
-  };
-
-  const validationSchema = Yup.object().shape({
-    search: Yup.string(),
-  });
-
-  const notify = () => toast.error("You need to enter the value!");
-
-  const handleSubmit = (values, actions) => {
-    const searchQuery = values.search.trim();
-    if (!searchQuery) {
-      notify();
-      setMoviesSearch([]);
-      setSearchParams({});
-      return;
-    }
-    searchParams.set("query", searchQuery);
-    setSearchParams(searchParams);
-    actions.resetForm();
+  const onSubmit = (newValue) => {
+    setSearchParams({ query: newValue });
   };
 
   useEffect(() => {
     const getData = async () => {
-      if (searchValue) {
-        try {
-          setIsError(false);
-          setErrorMessage("");
-          const data = await fetchMoviesSearch(searchValue);
-          if (data.length === 0) {
-            setMoviesSearch([]);
-            setErrorMessage("Nothing found. Please try again.");
-          } else {
-            setMoviesSearch(data);
-          }
-        } catch (error) {
-          setIsError(true);
-          setErrorMessage("Oops! Something went wrong. Please try again.");
-        }
+      try {
+        const data = await fetchSearchMovies(filterValue);
+        setMovies(data);
+      } catch (error) {
+        console.log(error);
       }
     };
     getData();
-  }, [searchValue]);
-
+  }, [filterValue]);
   return (
     <div>
-      <Formik
-        validationSchema={validationSchema}
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <Field
-            name="search"
-            placeholder="Search movies"
-            type="search"
-          ></Field>
-          <button type="submit">Search</button>
-        </Form>
-      </Formik>
-      {isError ? (
-        <p>{errorMessage}</p>
-      ) : moviesSearch.length === 0 ? (
-        <p>{errorMessage}</p>
-      ) : (
-        <MoviesList movies={moviesSearch} basicPath={""} />
-      )}
-      <Toaster />
+      <SearchBar filterValue={filterValue} onSubmit={onSubmit} />
+
+      <MovieList movies={movies} />
     </div>
   );
 };
